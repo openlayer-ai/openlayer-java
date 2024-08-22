57f193ca15e073829292313401fe5983447efe63
David Meadows <davidtmeadows@gmail.com>
2024-08-22 10:28:42 -0400

    fixup

---
+++ examples/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("application")
+    id("openlayer.java")
+}
+
+dependencies {
+    implementation(project(":openlayer-java"))
+}
+
+// Dynamically create a task for each example
+val examples = listOf("StreamData") // Add the names of your example classes here
+
+examples.forEach { example ->
+    tasks.create("run$example", org.gradle.api.tasks.JavaExec::class.java) {
+        group = "Examples"
+        description = "Run $example"
+        classpath = sourceSets.getByName("main").runtimeClasspath
+        mainClass.set("com.langsmith.example.$example")
+    }
+}
