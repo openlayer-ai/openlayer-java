f5c902411be41b07307760656b6d17329cb5a14e
David Meadows <davidtmeadows@gmail.com>
2024-08-22 10:28:42 -0400

    fixup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,4 +3,4 @@ rootProject.name = "openlayer-java-root"
 include("openlayer-java")
 include("openlayer-java-client-okhttp")
 include("openlayer-java-core")
-include("openlayer-java-example")
+include("examples")
